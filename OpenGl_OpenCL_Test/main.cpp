93572e7621a3f1a4c35e266d102bbbec2da5711b
Martin Pernica <pernica@pria.cz>
2014-09-22 19:21:23 +0200

    Initial Commit

---
+++ OpenGl_OpenCL_Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGl_OpenCL_Test
+//
+//  Created by Martin Pernica on 22.09.14.
+//  Copyright (c) 2014 Martin Pernica. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
